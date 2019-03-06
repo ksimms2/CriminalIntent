51befa3004ae1d6f7f1effe598460e9cdaaace4c
Kevin Simms <ksimms2@cnm.edu>
2019-03-06 08:01:44 -0700

    styled and added constraints to list_crime.xml

--- app/src/main/java/edu/cnm/deepdive/criminalintent/CrimeListFragment.java
+++ app/src/main/java/edu/cnm/deepdive/criminalintent/CrimeListFragment.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import java.util.List;
@@ -32,26 +33,33 @@ public class CrimeListFragment extends Fragment {
     return view;
   }
   private void updateUI() {
+
     CrimeLab crimeLab = CrimeLab.get(getActivity());
     List<Crime> crimes = crimeLab.getCrimes();
+
     mAdapter = new CrimeAdapter(crimes);
     crimeRecyclerView.setAdapter(mAdapter);
   }
   private class CrimeHolder extends RecyclerView.ViewHolder implements
       View.OnClickListener {
+
+    private ImageView mSolvedImageView;
     private TextView mTitleTextView;
     private TextView mDateTextView;
     private Crime mCrime;
+
     public CrimeHolder(LayoutInflater inflater, ViewGroup parent) {
       super(inflater.inflate(R.layout.list_item_crime, parent, false));
       itemView.setOnClickListener(this);
       mTitleTextView = (TextView) itemView.findViewById(R.id.crime_title);
       mDateTextView = (TextView) itemView.findViewById(R.id.crime_date);
+      mSolvedImageView = (ImageView) itemView.findViewById(R.id.crime_solved);
     }
     public void bind(Crime crime) {
       mCrime = crime;
       mTitleTextView.setText(mCrime.getTitle());
       mDateTextView.setText(mCrime.getDate().toString());
+      mSolvedImageView.setVisibility(crime.isSolved() ? View.VISIBLE : View.GONE);
     }
     @Override
     public void onClick(View view) {
